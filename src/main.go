94b4715cd5b12bd1348e71fd0eedecf71ff4cfa0
Harshita Vidapanakal <65297242+hvgh88@users.noreply.github.com>
2022-11-04 09:35:14 +0530

    Initializing data structures

--- src/main.go
+++ src/main.go
@@ -1,5 +1,9 @@
 package main
 
+//constants used:
+//	fileCount - Number of files system can support
+//	replicationFactor - 3 including the primary (1 + 2)
+//	opCount - To choose a random operation between read and write/append to facilitate automated selection
 const (
 	fileCount         int = 3
 	replicationFactor int = 3
